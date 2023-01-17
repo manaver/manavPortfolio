7faa170292faff195c92811248ab80383c22b024
Manav Verma <78682260+manaver@users.noreply.github.com>
2023-01-17 21:02:54 +0000

    Home Page created

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Manav Verma",
+  title: "Full Stack Developer",
+  email: "vermamanav117@gmail.com",
+  gitHub: "manaver",
+  linkedIn: "manav-verma-590a92204/",
+  twitter: "Manav1924",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -13,27 +13,30 @@ import PropTypes from "prop-types";
  * Home background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  *
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/server-wall.jpg";
+import image from "../images/name.png";
 
 const imageAltText = "woman holding laptop standing by server room with glass wall";
 
 const Home = ({ name, title }) => {
   return (
     <section id="home" className="dark">
-      <img className="background" src={image} alt="" />
-      <div style={{ position: "absolute", top: "30%", left: "2rem" }}>
-        <h1>{name}</h1>
-        <h2>{title}</h2>
-      </div>
-      <div style={{ position: "absolute", bottom: "8rem", left: "50%" }}>
-        <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
+      <div id="homeContentText">
+        <img className="" src={image} alt="" />
+        <div>
+          <h1>{name}</h1>
+          <h2>{title}</h2>
+          <div className="flexRow bodyBtn">
+            <h2 className="moreContentText">More</h2>
+            <img src={arrowSvg} className="white" style={{ height: "1.5rem", width: "1.5rem" }} alt={imageAltText} />
+          </div>
+        </div>
       </div>
     </section>
   );
